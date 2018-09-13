7ab8e719280281f720ba93888331eaf62a1756d7
Cam Iverson <camiver@gmail.com>
2018-09-13 16:08:47 -0500

    changed name of command as capital letters were causing an error, i think?

--- commands/socialBased.js
+++ commands/socialBased.js
@@ -1,9 +1,9 @@
 /*
-* Mod aboose
+* Social Based Guild
 */
 
 exports.run = async (client, message, args, level) => {
-    await message.channel.send(`Leaving has been on my mind lately and todays demonstration of disrespect was what finalized the decision. As the guild leader, I felt you should know my reasons. As a member of core, I have not felt like there was any way to progress within the guild. Things appear to just be social based and not merit based. The majority of leaders are not approachable and do not appear to take anything seriously. Some of the people that squirrel during raids are leaders and there have been a numbrer of reasons where I myself have had questions ignored by the leaders that were online on the time. I do appreciate your commanding and thank you for the raids, but this is not a guild environment I can be in.`);
+    await message.channel.send(`As a member of core i have not felt like there was any way to progress within the guild. Things appear to just be social based and not merit based. The majority of leaders are not approcahable and do not appear to take anything seriously. Some of the people that squirrel during raids are leaders and there have been a number of occassions where I myself have had questions ignored by the leaders that were online at the time. This just is not a guild environment i can be in.`);
 }
 
 exports.conf = {
@@ -14,8 +14,8 @@ exports.conf = {
 };
 
 exports.help = {
-    name: "socialBased",
+    name: "socialbased",
     category: "Miscellaneous",
-    description: "[DIS] is an ERP Social Progression Guild looking for 18+ brothel members.",
-    usage: "socialBased"
+    description: "DIS is an 18+ ERP Guild.",
+    usage: "socialbased"
 };
